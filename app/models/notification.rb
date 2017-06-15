7169252ab8515ba118dfd30f808417253885b489
Chris Arcand <chris@chrisarcand.com>
2017-06-15 12:02:12 -0500

    Fix Notification.type scope across subjects

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -20,7 +20,7 @@ class Notification < ApplicationRecord
   scope :starred,  -> { where(starred: true) }
 
   scope :repo,     ->(repo_name)    { where(repository_full_name: repo_name) }
-  scope :type,     ->(subject_type) { where(subject_type: subject_type) }
+  scope :type,     ->(subject_type) { joins(:subject).where(subjects: { type: subject_type }) }
   scope :reason,   ->(reason)       { where(reason: reason) }
   scope :unread,   ->(unread)       { where(unread: unread) }
   scope :owner,    ->(owner_name)   { where(repository_owner_name: owner_name) }
